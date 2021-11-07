b7f05cbe7a3ec36f2b3141c932396f531c240dba
khactrung0902 <khanhtrung13@gmail.com>
2021-11-07 21:58:21 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="title">vue-markdown-editor</h1>
+    <MarkedEditor/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MarkedEditor from './components/MarkedEditor'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MarkedEditor
   }
 }
 </script>
 
 <style>
+html,
+body,
 #app {
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.title {
+  text-align: center;
 }
 </style>
---
+++ src/components/MarkedEditor.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="md-editor">
+    <textarea v-model="html"></textarea>
+    <div v-html="compiledMarkdown"></div>
+  </div>
+</template>
+
+<script>
+import { marked } from 'marked';
+
+export default {
+  data () {
+    return {
+      html: "# hello markdown"
+    }
+  },
+  computed: {
+    compiledMarkdown() {
+      return marked(this.html);
+    }
+  }
+}
+</script>
+
+<style>
+#md-editor {
+  margin: 0;
+  height: 100%;
+}
+
+textarea,
+#md-editor div {
+  display: inline-block;
+  width: 49%;
+  height: 100%;
+  vertical-align: top;
+  box-sizing: border-box;
+  padding: 0 20px;
+  margin: 5px;
+  border: 1px solid #ccc;
+  overflow: auto;
+}
+
+textarea {
+  resize: none;
+  font-size: 14px;
+  padding: 20px;
+}
+
+code {
+  color: #f66;
+}
+</style>
